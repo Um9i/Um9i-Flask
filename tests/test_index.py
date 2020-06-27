b0a1607925ab50651fffb5aacf475ce78a1a3e88
Um9i <vulgarmessiah@gmail.com>
2020-06-27 19:59:19 +0100

    refactor tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,14 +1,4 @@
-import pytest
-from project import create_app
-
-
-@pytest.fixture
-def client():
-    app = create_app()
-    app.config["TESTING"] = True
-
-    with app.test_client() as client:
-        yield client
+from . import client
 
 
 def test_index(client):
