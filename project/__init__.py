2e6a10a0c1085cb90b1dd34b0eab9793d4a41afa
Um9i <vulgarmessiah@gmail.com>
2020-09-27 12:18:34 +0100

    add 404 page

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from werkzeug.utils import find_modules, import_string
 
 
 def create_app(config=None):
     app = Flask(__name__)
+    app.register_error_handler(404, page_not_found)
     register_blueprints(app)
     return app
+
+
+def page_not_found(e):
+  return render_template("404.html"), 404
 
 
 def register_blueprints(app):
