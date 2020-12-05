ce4bb2dc098eb0224fb298cbfadff73a589b0df6
Um9i <vulgarmessiah@gmail.com>
2020-12-05 17:01:49 +0000

    fix formatting

--- tests/test_404.py
+++ tests/test_404.py
@@ -3,4 +3,4 @@ from . import client
 
 def test_404(client):
     rv = client.get("/randomurl")
-    assert b"<h1 class="text-muted text-center py-5">404: Page Not Found</h1>" in rv.data
+    assert b'<h1 class="text-muted text-center py-5">404: Page Not Found</h1>' in rv.data
